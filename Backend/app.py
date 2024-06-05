2c11fa0083ba9075348211ffa0a1216ccd4b19f0
agusarellano <aarellano@fi.uba.ar>
2024-06-05 17:47:12 -0300

    #34 Agrego el endpoint (/cargarzona/<zona>)
    Devuelve en formato json las mascotas perdidas y centros de transito que se encuentren por zona. Se modifico tambien /tablademascotas

--- Backend/app.py
+++ Backend/app.py
@@ -15,7 +15,7 @@ def mostrar_tabla_de_mascotas():
     query = 'SELECT * FROM mascotas;'
 
     try: 
-       resultado=conexion.excute(text(query))
+       resultado=conexion.execute(text(query))
        conexion.close()
     except SQLAlchemyError as error:
        return jsonify({'error': str(error.__cause__)})
@@ -28,12 +28,53 @@ def mostrar_tabla_de_mascotas():
        mascota['sexo'] = fila.sexo
        mascota['raza'] = fila.raza
        mascota['detalles'] = fila.detalles
+       mascota['zona'] =fila.zona
+       mascota['calle']=fila.calle
+       mascota['altura']=fila.altura
        mascota['contacto'] = fila.contacto
-       mascota['EnBusqueda'] = fila.EnBusqueda
+       mascota['estado'] = fila.estado
        mascota['IDusuario'] = fila.IDusuario
        mascotas.append(mascota)
     return jsonify(mascotas)
 
+@app.route('/cargarzona/<zona>', methods=["GET"])
+def cargar_zona(zona):
+   conexion = engine.connect()
+   query_mascotas = f'SELECT * FROM mascotas WHERE zona = {zona}'
+   query_centros =  f'SELECT * FROM centros WHERE zona = {zona}'
+
+   try: 
+       resultado_mascotas=conexion.execute(text(query_mascotas))
+       resultado_centros=conexion.execute(text(query_centros))
+       conexion.close()
+   except SQLAlchemyError as error:
+       return jsonify({'error': str(error.__cause__)})
+   
+   if resultado_centros.rowcount!= 0 or resultado_mascotas.rowcount!= 0:
+      datos={
+         'centroTransito': [],
+         'mascotasPerdidas': [] 
+            }
+      for fila in resultado_mascotas:
+         datos['mascotasPerdidas'].append({
+            'especie':fila.especie,
+            'raza':fila.raza,
+            'zona':fila.zona,
+            'calle':fila.calle,
+            'altura':fila.altura
+         })
+      for fila in resultado_centros:
+         datos['centroTransito'].append({
+            'nombre':fila.nombre,
+            'zona':fila.zona,
+            'calle':fila.calle,
+            'altura': fila.altura,
+            'descripcion':fila.descripcion,
+            'contacto':fila.contacto
+         })
+      return jsonify(datos)
+   return jsonify({'mensaje': 'No hay animales ni centros de animales en transito por esta zona'}, 404)
+   
 
 if __name__ == '__main__':
   app.run(debug=True, port=PORT)
