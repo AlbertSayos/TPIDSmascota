c6b37b80b4d31c1e650aaabebdf68d33011740f8
AlbertSayos <asayos@fi.uba.ar>
2024-06-06 16:00:52 -0300

    marge con la rama de franco y agrego el mapa

--- Frontend/app.py
+++ Frontend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, render_template, Response
 import requests  # Se utiliza para hacer consultas a APIs externas
 import os  # Se utiliza para interactuar con variables de entorno
 from dotenv import load_dotenv  # Se utiliza para cargar variables de entorno desde un archivo .env
-
+api_key = os.getenv('APIKEY') #api de google cloud
 PORT = 8080
 
 load_dotenv()  # Carga las variables de entorno desde el archivo .env si existe
@@ -14,22 +14,23 @@ BackendLink = os.getenv('backend_link')  # Obtiene el valor de la variable de en
 @app.route('/')
 def index():
     #respuesta = requests.get(f'{BackendLink}')  # Realiza una solicitud GET a la URL almacenada en 'BackendLink'
-    api_key = os.getenv('APIKEY') #api de google cloud
     
-    return render_template('home.html',api_key=api_key)  # Devuelve la respuesta JSON de la solicitud realizada
+    return render_template('home.html',api_key=api_key)
 
 @app.route('/map')
 def map():
-    return render_template('map.html')
+    return render_template('map.html',api_key=api_key)
 
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html',api_key=api_key)
+
+@app.route('/registrar')
+def registrar():
+    return render_template('registrar.html')
 
 @app.route('/cargarMapa')
 def cargarMapa():
-    api_key = os.getenv('APIKEY') #api de google cloud
-    print("lo pase"+ api_key)
     return render_template('mapDeEjemplo.html', api_key=api_key)
 
 if __name__ == '__main__':
