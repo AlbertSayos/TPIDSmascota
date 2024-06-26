7d746f4e9b00d421bb63c11967815832ae6d1f6a
Alexote-byte <lezcanof33@gmail.com>
2024-06-26 17:30:58 -0300

    actualice el buscador y el filtro para visualizar el estado

--- Frontend/app.py
+++ Frontend/app.py
@@ -139,17 +139,20 @@ def buscadas():
         "id": "",
         "especie": "",
         "raza": "",
-        "sexo": ""
+        "sexo": "",
+        "estado": ""
     }
     if request.method == "POST":
         especie = request.form.get("mespecie")
         raza = request.form.get("mraza")
         sexo = request.form.get("msexo")
+        estado = request.form.get("mestado")
 
         datos = {
             "especie": especie,
             "raza": raza,
-            "sexo": sexo
+            "sexo": sexo,
+            "estado":estado
         }
     tabla = requests.post(f'{BackendLink}/buscarmascotas', json=datos)
     if tabla.status_code == 200:
